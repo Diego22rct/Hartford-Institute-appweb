1aae007f65d71ad0c1aeb092dbc317ee6a87fc84
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-05-07 13:00:04 -0500

    chore: Update header structure and styling in App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,34 @@
 </script>
 
 <template>
-  <h1>header</h1>
-  <router-view></router-view>
+  <header>
+      <pv-toolbar style="border-radius: 3rem; padding: 1rem 1rem 1rem 1.5rem">
+        <template #start>
+            <div class="flex align-items-center gap-2">
+                <pv-image src="/public/HIGN-logo.png" alt="HIGN Logo" height="50" />
+            </div>
+        </template>
+
+        <template #end>
+            <div class="flex align-items-center gap-2">
+              <router-link to="/home">
+                <pv-button label="Home" text plain severity="contrast" />
+              </router-link>
+              <router-link to="/nursing/mental-state-exams">
+                <pv-button label="Mental exams" text plain severity="contrast" />
+              </router-link>
+            </div>
+        </template>
+    </pv-toolbar>
+  </header>
+  <main>
+    <router-view />
+  </main>
 </template>
 
 <style scoped>
-
+header {
+  margin-bottom: 1rem;
+  border-bottom: 1px solid #e0e0e0;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -15,7 +15,18 @@ import 'primeicons/primeicons.css';
 // PrimeVue Material Design Theme
 import 'primevue/resources/themes/mdc-light-indigo/theme.css';
 
+
+// PrimeVue Components
+import Toolbar from 'primevue/toolbar';
+import Button from "primevue/button";
+import Avatar from "primevue/avatar";
+import Image from 'primevue/image';
+
 createApp(App)
     .use(PrimeVue, {ripple: true})
     .use(router)
+    .component('pv-toolbar', Toolbar)
+    .component('pv-button', Button)
+    .component('pv-avatar', Avatar)
+    .component('pv-image', Image)
     .mount('#app')
